358dd027cd71f45a720fe7f4284708aa3d59fd25
avihai119 <avihi119@gmail.com>
2025-11-02 19:23:58 +0200

    Added shooting and shooting delay, modified: main.py, asteroid.py, shot.py, player.py, constants.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 
 def main():
@@ -19,10 +20,12 @@ def main():
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatables, drawables)
     Asteroid.containers = (asteroids, updatables, drawables)
     AsteroidField.containers = (updatables)
+    Shot.containers = (shots, updatables, drawables)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -34,15 +37,18 @@ def main():
 
         updatables.update(dt)
 
-        screen.fill("black")
-
-        for drawable in drawables:
-            drawable.draw(screen)
 
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 print("Game Over!")
                 sys.exit()
+
+        screen.fill("black")
+
+        for drawable in drawables:
+            drawable.draw(screen)
+
+
         
         pygame.display.flip()
 
